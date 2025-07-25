00a21ca0e21f15096cb7aa9d473db593eb28b04d
“Ivan <ivangalindoangulo@gmail.com>
2025-07-25 16:38:44 -0500

    Create Form
    Crear lista de espera

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind';
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: "https://inverzzo.com",
+  output: 'static',
+  integrations: [tailwind()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
