e4ac78d1b8f4a0834a13f1224a78994674c78718
Ivan Galindo <ivangalindoangulo@gmail.com>
2026-05-09 08:25:59 -0500

    fijar astro config para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://inverzzo.com",
-  output: 'server',
+  site: "https://ivangalindoangulo.github.io",
+  base: "/inverzzo-starter-landing-page",
+  output: "static",
   integrations: [tailwind()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
 });
